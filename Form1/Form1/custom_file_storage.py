e989465d1f38c4795630ec95d0993c73e3792efc
gopal  goyal <44091979+gopalgoyal1999@users.noreply.github.com>
2020-06-01 17:04:44 +0530

    ..

---
+++ Form1/Form1/custom_file_storage.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class MediaFilesStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
+
+class StaticFilesStorage(S3BotoStorage):
+    location = settings.STATICFILES_LOCATION
